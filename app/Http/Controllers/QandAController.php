0657f934a4d95b2abc60f65252136bb81fbc42b9
MarionRosete <marion.rosete@fligno.com>
2022-10-05 21:08:26 +0800

    QandA -mcr

---
+++ app/Http/Controllers/QandAController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\QandA;
+use Illuminate\Http\Request;
+
+class QandAController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\QandA  $qandA
+     * @return \Illuminate\Http\Response
+     */
+    public function show(QandA $qandA)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\QandA  $qandA
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(QandA $qandA)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\QandA  $qandA
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, QandA $qandA)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\QandA  $qandA
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(QandA $qandA)
+    {
+        //
+    }
+}
