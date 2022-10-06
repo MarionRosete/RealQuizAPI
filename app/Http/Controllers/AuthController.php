9bb03611c00d1bed729c2823ef20dd2981fca1b8
MarionRosete <marion.rosete@fligno.com>
2022-10-06 13:53:14 +0800

    #1 login and register functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,23 +12,38 @@ class AuthController extends Controller
     
 
    
-    public function register()
+    public function register(Request $request)
     {
-        
+       
+        $request->validate([
+            'email' => 'required|email|max:50|unique:users',
+            'name'=>'required|max:50',
+            'password' => 'required|min:8|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $request['name'],
+            'email'=> $request['email'],
+            'password' =>  bcrypt($request['password'])
+        ]);
+       
+        return ['token' =>  $user->createToken('auth_token')->plainTextToken, "user"=>$user];
+
+
     }
 
    
     public function login(Request $request)
     {
         $user = User::where('email', $request->email)->first();
- 
+      
         if (! $user || ! Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
                 'msg' => ['The provided credentials are incorrect.'],
             ]);
         }
      
-        return $user->createToken($request->device_name)->plainTextToken;
+        return ['token'=>$user->createToken('auth_token')->plainTextToken, 'user'=>$user];
        
     }
 
